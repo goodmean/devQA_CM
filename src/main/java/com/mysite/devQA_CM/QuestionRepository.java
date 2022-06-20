880c14507dfa2eda466e159afcfa498c79264758
goodmean <zizigoo12@gmail.com>
2022-06-20 15:52:04 +0900

    findBySubjectAndContent

--- src/main/java/com/mysite/devQA_CM/QuestionRepository.java
+++ src/main/java/com/mysite/devQA_CM/QuestionRepository.java
@@ -6,4 +6,6 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
 	Question findBySubject(String subject);
 	// 리포지터리 객체의 메서드가 실행될때 JPA 가 해당 메서드명을 분석하여 쿼리를 만들고 실행한다.
 	// findBy + 엔티티의 속성명(예:findBySubject)과 같은 리포지터리 메서드를 작성하면 해당 속성의 값으로 데이터를 조회할수 있다.
+
+	Question findBySubjectAndContent(String subject, String content);
 }
