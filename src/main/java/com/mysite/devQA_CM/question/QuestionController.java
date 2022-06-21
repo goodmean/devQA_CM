e111159f1c3f6d802464fa36f2ff167be811c737
goodmean <zizigoo12@gmail.com>
2022-06-21 17:44:58 +0900

    QuestionService

--- src/main/java/com/mysite/devQA_CM/question/QuestionController.java
+++ src/main/java/com/mysite/devQA_CM/question/QuestionController.java
@@ -11,11 +11,11 @@ import java.util.List;
 @Controller
 public class QuestionController {
 
-	private final QuestionRepository questionRepository;
+	private final QuestionService questionService;
 
 	@RequestMapping("/question/list")
 	public String list(Model model) {
-		List<Question> questionList = this.questionRepository.findAll();
+		List<Question> questionList = this.questionService.getList();
 		model.addAttribute("questionList", questionList); // Model 객체에 "questionList" 라는 이름으로 저장
 		// Model 객체에 값을 담아두면 템플릿에서 그 값을 사용할 수 있다.
 		return "question_list";
