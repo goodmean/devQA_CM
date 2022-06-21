32dddf5c28fdac2fc736f85a8a771454aec87963
goodmean <zizigoo12@gmail.com>
2022-06-21 17:25:30 +0900

    ROOT URL 호출시 질문 목록 출력

--- src/main/java/com/mysite/devQA_CM/MainController.java
+++ src/main/java/com/mysite/devQA_CM/MainController.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-	@RequestMapping("/") // 요청된 URL 과의 매핑을 담당
+	@RequestMapping("/sbb") // 요청된 URL 과의 매핑을 담당
 	@ResponseBody // URL 요청에 대한 응담으로 문자열을 리턴
 	public String index() {
 		return "index";
+	}
+
+	@RequestMapping("/")
+	public String root() {
+		return "redirect:/question/list";
 	}
 }
