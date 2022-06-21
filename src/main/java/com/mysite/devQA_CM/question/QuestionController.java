4113b93c731e90e1db6ed43dee339877ba9dd151
goodmean <zizigoo12@gmail.com>
2022-06-21 18:20:21 +0900

    질문 상세

--- src/main/java/com/mysite/devQA_CM/question/QuestionController.java
+++ src/main/java/com/mysite/devQA_CM/question/QuestionController.java
@@ -3,6 +3,7 @@ package com.mysite.devQA_CM.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,5 +20,12 @@ public class QuestionController {
 		model.addAttribute("questionList", questionList); // Model 객체에 "questionList" 라는 이름으로 저장
 		// Model 객체에 값을 담아두면 템플릿에서 그 값을 사용할 수 있다.
 		return "question_list";
+	}
+
+	@RequestMapping(value = "/question/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		Question question = this.questionService.getQuestion(id);
+		model.addAttribute("question", question);
+		return "question_detail";
 	}
 }
