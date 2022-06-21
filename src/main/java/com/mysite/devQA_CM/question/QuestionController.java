0fcfab9f208ff2fc7b4131ec35a0639badae9cb6
goodmean <zizigoo12@gmail.com>
2022-06-21 17:21:08 +0900

    질문 목록을 템플릿을 이용하여 출력

---
+++ src/main/java/com/mysite/devQA_CM/question/QuestionController.java
@@ -0,0 +1,23 @@
+package com.mysite.devQA_CM.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+
+	private final QuestionRepository questionRepository;
+
+	@RequestMapping("/question/list")
+	public String list(Model model) {
+		List<Question> questionList = this.questionRepository.findAll();
+		model.addAttribute("questionList", questionList); // Model 객체에 "questionList" 라는 이름으로 저장
+		// Model 객체에 값을 담아두면 템플릿에서 그 값을 사용할 수 있다.
+		return "question_list";
+	}
+}
