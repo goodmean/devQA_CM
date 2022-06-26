07b4ac42dca7e9dae50bd29ec37e1a5377517a5b
goodmean <zizigoo12@gmail.com>
2022-06-26 22:58:57 +0900

    스프링 시큐리티 설정

---
+++ src/main/java/com/mysite/devQA_CM/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.mysite.devQA_CM;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/**").permitAll() // 모든 인증되지 않은 요청
+		;
+	}
+}
