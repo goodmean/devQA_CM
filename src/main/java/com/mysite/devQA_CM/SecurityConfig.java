ad7d65cb78e2970cfb69012aa0f526b156cd3cd2
goodmean <zizigoo12@gmail.com>
2022-06-26 23:15:28 +0900

    유저 리포지터리, 유저 서비스

--- src/main/java/com/mysite/devQA_CM/SecurityConfig.java
+++ src/main/java/com/mysite/devQA_CM/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.mysite.devQA_CM;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +15,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/**").permitAll() // 모든 인증되지 않은 요청
 		;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
