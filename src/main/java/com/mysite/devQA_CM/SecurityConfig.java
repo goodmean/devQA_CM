50d8015a1877c8e317f3147062bf7a2cd195153f
goodmean <zizigoo12@gmail.com>
2022-06-27 21:29:15 +0900

    로그인 기능 구현

--- src/main/java/com/mysite/devQA_CM/SecurityConfig.java
+++ src/main/java/com/mysite/devQA_CM/SecurityConfig.java
@@ -1,16 +1,23 @@
 package com.mysite.devQA_CM;
 
+import com.mysite.devQA_CM.user.UserSecurityService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final UserSecurityService userSecurityService;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/**").permitAll() // 모든 인증되지 않은 요청
@@ -19,6 +26,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 					.loginPage("/user/login")
 					.defaultSuccessUrl("/")
 		;
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
 	}
 
 	@Bean
