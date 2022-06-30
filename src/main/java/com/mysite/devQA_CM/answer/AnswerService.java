28d47cec601484f5eaf9e96007e52b620b0cc23b
goodmean <zizigoo12@gmail.com>
2022-06-30 23:57:11 +0900

    답변 수정 기능 구현

--- src/main/java/com/mysite/devQA_CM/answer/AnswerService.java
+++ src/main/java/com/mysite/devQA_CM/answer/AnswerService.java
@@ -1,11 +1,13 @@
 package com.mysite.devQA_CM.answer;
 
+import com.mysite.devQA_CM.DataNotFoundException;
 import com.mysite.devQA_CM.question.Question;
 import com.mysite.devQA_CM.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -22,5 +24,20 @@ public class AnswerService {
 		answer.setAuthor(author);
 		this.answerRepository.save(answer);
 
+	}
+
+	public Answer getAnswer(Integer id) {
+		Optional<Answer> answer = this.answerRepository.findById(id);
+		if (answer.isPresent()) {
+			return answer.get();
+		} else {
+			throw new DataNotFoundException("answer not found");
+		}
+	}
+
+	public void modify(Answer answer, String content) {
+		answer.setContent(content);
+		answer.setModifyDate(LocalDateTime.now());
+		this.answerRepository.save(answer);
 	}
 }
